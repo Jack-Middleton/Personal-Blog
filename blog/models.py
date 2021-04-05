61daf9985f2f3efa1695e757961354eaaa76c93a
Jack Middleton <jackmiddleton96@hotmail.co.uk>
2021-04-05 18:10:17 +0100

    Formatting

--- blog/models.py
+++ blog/models.py
@@ -5,19 +5,25 @@ from django.utils import timezone
 
 class Post(models.Model):  # class is the keyword for defining a class
     #  models.Model makes the class inherit from the Models class and get treated as a model to store in a database
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # link to another model
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )  # link to another model
     # cascade tells the database to delete everything in a cascading fashion
     # between linked databases
-    title = models.CharField(max_length=200)  # How you define text with a limited number of characters
-    text = models.TextField() # Long text without limit
+    title = models.CharField(
+        max_length=200
+    )  # How you define text with a limited number of characters
+    text = models.TextField()  # Long text without limit
     created_date = models.DateTimeField(default=timezone.now)  # this is date and time
     published_date = models.DateTimeField(blank=True, null=True)
     # allows the date and time of published_date to be either blank or null
 
-#  functions indented so that they belong to the class
+    #  functions indented so that they belong to the class
 
     def publish(self):
-        self.published_date = timezone.now()  # Sets the published_date date and time upon website publish
+        self.published_date = (
+            timezone.now()
+        )  # Sets the published_date date and time upon website publish
         self.save()
 
     def __str__(self):  # dunder method that overwrites the str method and
