ed83ad359b4bbc0afcd7e5d5bdba2d32646dbb51
Jack Middleton <jackmiddleton96@hotmail.co.uk>
2021-04-26 11:05:50 +0100

    added link to linkedin page

--- blog/views.py
+++ blog/views.py
@@ -26,10 +26,10 @@ def post_new(request):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect("post_detail", pk=post.pk)
     else:
         form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, "blog/post_edit.html", {"form": form})
 
 
 def post_edit(request, pk):
@@ -41,7 +41,7 @@ def post_edit(request, pk):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect("post_detail", pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, "blog/post_edit.html", {"form": form})
